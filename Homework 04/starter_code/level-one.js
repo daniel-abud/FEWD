7ae3eb6a5bfab2e16069dc0ab06d57a9413265d5
daniel-abud <daniel-abud@github.com>
2022-10-09 13:06:05 -0700

    hw4 start

--- Homework 04/starter_code/level-one.js
+++ Homework 04/starter_code/level-one.js
@@ -5,7 +5,29 @@
 let score = 0;
 let ballEl = document.querySelector('.js-ball');
 let scoreEl = document.querySelector('.js-score');
+let point = 10;
 
 function declareWinner() {
   document.body.classList.add('game-over');
 }
+
+// create function if click listener = true then add 10 points
+function addPoints(scoreEl){
+  if (ballEl == true) {
+    scoreEl = score + '10'
+    // document.body.className.replace('.js-score',score)
+  }
+  console.log("10")
+}
+
+// call the function
+ballEl.addEventListener('click',addPoints);
+
+
+
+// if total = 100 points then add paragraph with information ready to next level
+// const newParagraph = document.createElement("p")
+// newParagraph.innerText = "Ready for the next level"
+// document.body.appendChiled(newParagraph);
+
+// add image celebration
--- Lesson 08/05_traffic_light/starter_code/script.js
+++ Lesson 08/05_traffic_light/starter_code/script.js
@@ -1,2 +1,33 @@
 // Try making the traffic light work - Think through the problem BEFORE you code!
 // You'll use addEventListener and getElementById if you're doing it right...
+
+
+// get buttons
+
+let stopButton = document.getElementById('stopButton');
+let slowButton = document.getElementById('slowButton');
+let goButton = document.getElementById('goButton');
+
+// get lights
+
+let stopLight = document.getElementById('stopLight');
+let slowLight = document.getElementById('slowLight');
+let goLight = document.getElementById('goLight');
+
+
+// click stop light red on
+
+stopButton.addEventListener('click',function(){
+    document.body.classList.toggle('stopLight')
+    document.body.classList.remove('slowLight')
+    document.body.classList.remove('goLight')
+})
+
+
+// click slow turn yellow on
+
+
+
+// click go turn green on
+
+
