57dc8842f31c26b0d618e458a90fc9b2e0587bf7
daniel-abud <daniel-abud@github.com>
2022-10-12 20:49:01 -0700

    hw4 level two

--- Homework 04/starter_code/level-one.js
+++ Homework 04/starter_code/level-one.js
@@ -7,21 +7,31 @@ let ballEl = document.querySelector('.js-ball');
 let scoreEl = document.querySelector('.js-score');
 let point = 10;
 
+// create function if click listener = true then add 10 points
+// function addPoints(scoreEl){
+//   if (ballEl == true) {
+//     scoreEl = score + '10'
+//     document.body.className.replace('.js-score',score)
+//   }
+//   console.log("10")
+// }
+
+// call the function
+ballEl.addEventListener('click',
+  function(){
+    score = score + point; 
+    if(score < 20){
+      scoreEl.innerHTML = score;
+    }
+    else {
+      declareWinner();
+    }
+  }
+  );
+
 function declareWinner() {
   document.body.classList.add('game-over');
 }
-
-// create function if click listener = true then add 10 points
-function addPoints(scoreEl){
-  if (ballEl == true) {
-    scoreEl = score + '10'
-    // document.body.className.replace('.js-score',score)
-  }
-  console.log("10")
-}
-
-// call the function
-ballEl.addEventListener('click',addPoints);
 
 
 
